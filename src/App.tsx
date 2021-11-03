a3ad0a620c392404c28638b7878b9df96b0d1f69
Samila Castro <samilav1997@gmail.com>
2021-11-03 19:49:46 -0300

    Adiciona novos components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard/>
       <GlobalStyle/>
     </>
   );
