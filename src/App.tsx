12b71acbbf6d7d8aac18469a8a0e1e2e0795a32b
Samila Castro <samilav1997@gmail.com>
2021-11-02 21:37:14 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { GlobalStyle } from './styles/global';
 export function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <GlobalStyle/>
     </div>
   );
 }
