a614e9eb783119a2e08c414e5573de54d225aafc
Samila Castro <samilav1997@gmail.com>
2021-11-20 22:52:24 -0300

    Adiciona estilo global ao button

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -73,6 +73,21 @@ export const GlobalStyle = createGlobalStyle`
 
     }
 
+    .react-modal-close {
+      position: absolute;
+      right: 1.5rem;
+      top: 1.5rem;
+      border: 0;
+      background: transparent;
+
+      transition: filter 0.2s;
+
+      &:hover {
+        filter: brightness(0.8);
+      }
+
+    }
+
 
 
 `
