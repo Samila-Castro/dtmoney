afa04bde9f0803cfd9907c7e8214c720d3eb02e2
Samila Castro <samilav1997@gmail.com>
2021-11-20 21:24:49 -0300

    ADicionando estrutura do Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 
 interface NewTansactionModalProps {
@@ -7,8 +8,28 @@ interface NewTansactionModalProps {
 }
 export function NewTransactionModal({ isOpen, onRequestClose}: NewTansactionModalProps) {
     return (
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+        <Modal isOpen={isOpen} 
+        onRequestClose={onRequestClose}>
+
+        <Container>
         <h2>Cadastrar transação</h2>
+
+        <input type="text" 
+        placeholder="Título"
+        />
+
+        <input type="number" 
+        placeholder="Valor"
+        />
+        <input type="text" 
+        placeholder="Categoria"
+        />
+
+        <button type="submit">
+            Cadastrar
+        </button>
+
+        </Container>
   </Modal>
     )
 }
