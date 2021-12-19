915806dce569f7b1f11e03e377d6ae540712e2dc
Samila Castro <samilav1997@gmail.com>
2021-12-19 15:12:52 -0300

    Cria entidade transaction no bd interno do mirage

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: { //bd interno do miragejs
+    transaction: Model //tabela/entidade !!! SEmpre começa vazio
+    
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-12 11:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions' , () => {
-      return[
-        {
-          id:1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(),
+      return this.schema.all('transaction');
+    })
 
-        }
-      ]
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data); //first param: model onde será inserido e os dados que serão inseridos
     })
   }
 })
